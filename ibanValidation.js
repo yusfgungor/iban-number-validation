ad04ad4d3eb622eb8348a9ebb4417d69b1fcc819
yusfgungor <yusfgungor@gmail.com>
2019-10-16 21:08:17 +0300

    charactersMatchAndChange function is refactored

--- ibanValidation.js
+++ ibanValidation.js
@@ -1,39 +1,8 @@
 // Variables
-
-const numericRegex = /^[0-9]*$/;
 const MOD_START_VAL = 0;
 const MOD_MUST_EQUAL_TO_ONE = 1;
-const charactersConversionArray = {
-  A: '10',
-  B: '11',
-  C: '12',
-  D: '13',
-  E: '14',
-  F: '15',
-  G: '16',
-  H: '17',
-  I: '18',
-  J: '19',
-  K: '20',
-  L: '21',
-  M: '22',
-  N: '23',
-  O: '24',
-  P: '25',
-  Q: '26',
-  R: '27',
-  S: '28',
-  T: '29',
-  U: '30',
-  V: '31',
-  W: '32',
-  X: '33',
-  Y: '34',
-  Z: '35'
-}
 
 // Functions
-
 const setCharactersLocations = ibanValue => {
   // The first four characters of the IBAN are truncated and thrown to the right of the number.
   // ibanValue = TR47 0000 1001 0000 0350 9300 01
@@ -45,13 +14,14 @@ const setCharactersLocations = ibanValue => {
 }
 
 const charactersMatchAndChange = value => value
-  .split('')
-  .map((item) => numericRegex.test(item) ? item : charactersConversionArray[item])
-  .join('');
+  .split('') // ['0','0','0','0','1','0','0','1','0','0','0','0','0','3','5','0','9','3','0','0','0','1','T','R','4','7']
+  .map((item) => parseInt(item, 36)) // [0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 3, 5, 0, 9, 3, 0, 0, 0, 1, 29, 27, 4, 7]
+  .join('') // 0000100100000350930001292747
 
 const getMod9710 = (value) => {
   const valueToArray = value.split('');
-
+  // [0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 3, 5, 0, 9, 3, 0, 0, 0, 1, 2, 9, 2, 7, 4, 7]
+  
   return valueToArray.reduce((accumulator, currentValue) =>
     (((accumulator * 10) + Number(currentValue)) % 97 ), MOD_START_VAL);
 }
